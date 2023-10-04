f2502aabc13b01abcbd5527e949de881b1d0ce64
49rip <146723162+49rip@users.noreply.github.com>
2023-10-04 21:52:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<html>
-<head>
-  <title>Document</title>
-</head>
-<body>
 
-<?php
-
-include("discord.php");
-$sendembed = New Discord();
-
-//Executes the function
-$sendembed->Visitor();
-?>
-
-</body>
-</html>
